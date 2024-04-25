9d1014dfbcb9b5e68035f649b678acef940485ad
RubyBlade <aliakseikrukouski@gmail.com>
2024-04-25 22:52:27 +0200

    Updates the total calculation for order

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,14 +3,14 @@ class Order < ApplicationRecord
   before_validation :set_total!
   has_many :placements, dependent: :destroy
   has_many :products, through: :placements
+  validates_with EnoughProductsValidator
 
   belongs_to :user
   validates :total, numericality: { greater_than_or_equal_to: 0}
   validates :total, presence: true
-  validates_with EnoughProductsValidator
 
   def set_total!
-    self.total = products.map(&:price).sum
+    self.total = self.placements.map{ |placement| placement.product.price * placement.quantity }.sum
   end
 
   def build_placements_with_product_ids_and_quantities(product_ids_and_quantities)
